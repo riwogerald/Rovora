2cfcdc3b9e73ab7765772107b878a60d6b1877e2
Gerald Riwo <167013728+riwogerald@users.noreply.github.com>
2025-06-28 22:37:32 +0300

    Updated route_meta_data.json

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -3,13 +3,21 @@ export { matchers } from './matchers.js';
 export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
-	() => import('./nodes/2')
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2]
+		"/": [2],
+		"/games": [3],
+		"/games/[slug]": [4],
+		"/profile": [5],
+		"/wishlist": [6]
 	};
 
 export const hooks = {
---
+++ .svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/games/[slug]/+page.svelte";
