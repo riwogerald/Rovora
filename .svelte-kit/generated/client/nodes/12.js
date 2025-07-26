95407a8db6d24bf603422971a62530b41c8840d6
riwogerald <riwo.gerald@outlook.com>
2025-07-26 21:09:02 +0300

    Update 12.js

--- .svelte-kit/generated/client/nodes/12.js
+++ .svelte-kit/generated/client/nodes/12.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/(auth)/verify-email-notice/+page.svelte";
+export { default as component } from "../../../../src/routes/(auth)/verify-email/+page.svelte";
